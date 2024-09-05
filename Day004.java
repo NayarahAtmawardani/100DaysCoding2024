10fcecb90ebcad6435b7d1f0d719cb2804094e35
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-05 11:46:38 +0700

    Update Day004.java

--- Day004.java
+++ Day004.java
@@ -1,8 +1,9 @@
-
 package Day004;
 public class java {
     public static void main(String[] args) {
-        System.out.printf("halo semua perkenalkan:"); 
-        System.out.printf("nama saya %s dan saya berumur %d tahun.", "Nayarah Atmawardani",18);
+        System.out.printf("halo semua perkenalkan:%s%n","Nayarah"); 
+        System.out.printf("umur saya adalah %d %n" ,18);
+        System.out.printf("bb saya seberat %f%n",41.0);
+        System.out.printf("jenis kelamin saya adalah %c%n",'P');
     }
 }
