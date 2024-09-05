08082fcf67739a879c856eb07737b0685fedba89
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-05 09:55:25 +0700

    Create day004.java

---
+++ day004.java
@@ -0,0 +1,8 @@
+
+package Day004;
+public class java {
+    public static void main(String[] args) {
+        System.out.printf("halo semua perkenalkan:"); 
+        System.out.printf("nama saya %s dan saya berumur %d tahun.", "Nayarah Atmawardani",18);
+    }
+}
