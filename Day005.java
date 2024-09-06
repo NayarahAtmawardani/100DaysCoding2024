8608ec60253bbc943c2590a5f22ca91d6f720577
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-06 09:08:55 +0700

    Create Day005.java

---
+++ Day005.java
@@ -0,0 +1,11 @@
+public class day005 {
+    public static void main(String[] args) {
+        String nama = "naya";
+        int umur = 18;
+        System.out.printf("nama: %s, umur: %d ", nama, umur);
+        //kalo garing dua untuk komentar satu kolom
+        /*garing satu dengan bintang untuk lebih dari satu kolom*/
+     
+    }
+  
+}
