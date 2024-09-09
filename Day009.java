b02ead56992d6e04451626e1c2267d240f8584a1
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-10 06:03:53 +0700

    Create Day009.java

---
+++ Day009.java
@@ -0,0 +1,16 @@
+
+public class day009 {
+  public static void main (String []args){
+     final String nama = "nayarah";
+      System.out.println("Nama = " + nama);
+      
+      final int umur =18;
+      System.out.println("Umur = " + umur);
+      
+      final byte UkuranSepatu = 37;
+      System.out.println("UkuranSepatu = " + UkuranSepatu);
+      
+      final double bb =45.0;
+      System.out.println("bb = " + bb);
+  }  
+}
