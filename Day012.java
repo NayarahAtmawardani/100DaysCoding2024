4d247820e9d1d17c61a501273fffba92a961559b
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-13 05:08:08 +0700

    Create Day012.java

---
+++ Day012.java
@@ -0,0 +1,11 @@
+
+public class day012 {
+    public static void main(String[] args) {
+        int minimum = -2147483;
+        int maksimum = 2147483;
+        
+        System.out.println("minimum = " + minimum);
+        System.out.println("maksimum = " + maksimum);
+    }
+  
+}
