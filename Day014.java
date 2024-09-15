f129423ea18703c33e3a6cb968e6155f9314cd5d
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-15 08:15:31 +0700

    Create Day014.java

---
+++ Day014.java
@@ -0,0 +1,10 @@
+
+public class typedatalong {
+    public static void main(String[] args) {
+      /*Tipe data long adalah tipe data primitif yang digunakan untuk menyimpan bilangan bulat dengan ukuran lebih besar.
+        Tipe data long memiliki ukuran 64-bit (8 byte) dan untuk strukturnya ada package,public class dan method main. */
+        long panjang = 100;
+        System.out.println("panjang = "+ panjang);
+    }
+  
+}
