8ebb69426cc0c6a4ff0e842ec019aca4461c2bc6
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-16 09:54:20 +0700

    Create day15.java

---
+++ day15.java
@@ -0,0 +1,9 @@
+public class day015 {
+    public static void main(String[] args) {
+    /*double adalah salah satu tipe data primitif di Java,memiliki 8 byte penyimpanan dan juga untuk menyimpan bilangan desimal*/
+    
+    double tinggiBadan = 149.0;
+        System.out.println("saya memiliki tinggi badan = "+tinggiBadan);
+    }
+  
+}
