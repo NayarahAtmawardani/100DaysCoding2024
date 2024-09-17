3b76842035d1d60548cfb63820ea18d3a69b19bb
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-18 06:05:35 +0700

    Create Day017.java

---
+++ Day017.java
@@ -0,0 +1,11 @@
+
+package day17;
+
+public class day17 {
+    public static void main(String[] args) {
+        // char merupakan salah satu tipe data primitif yang menyimpan karakter tunggal
+        char jenisKelamin = 'P';
+        System.out.println("jenis kelamin : " + jenisKelamin);
+    }
+  
+}
