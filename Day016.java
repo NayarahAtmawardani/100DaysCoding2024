c7dd973cdfba9f0b3af76e58bf6154e54fbe5e96
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-17 09:00:23 +0700

    Create Day016.java

---
+++ Day016.java
@@ -0,0 +1,11 @@
+
+public class day016 {
+    public static void main(String[] args) {
+        /* tipe data float adalah tipe data primitif yang
+           menyimpan bilangan desimal  */ 
+        
+        float beratbadan = 45.0f;
+        System.out.println("berat badan : " + beratbadan + "kg");
+    }
+   
+}
