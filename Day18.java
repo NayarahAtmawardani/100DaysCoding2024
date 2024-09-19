9fb55d01ede98b528b5c50574360e7a89ce0c4da
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-19 08:05:49 +0700

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,10 @@
+
+public class day18 {
+    public static void main(String[] args) {
+        //boolean merupakan tipe data primitif yang hanya memiliki dua nilai yaitu true and false
+        boolean codingIsFun = true;
+        System.out.println("coding is fun : " + codingIsFun);
+       
+    }
+   
+}
