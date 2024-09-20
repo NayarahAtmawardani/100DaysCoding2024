d5f0512b8fe134ca9d3a9d55e79fdaaa6bc1b408
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-20 09:17:56 +0700

    Create Day019.java

---
+++ Day019.java
@@ -0,0 +1,11 @@
+
+public class day19 {
+    public static void main(String[] args) {
+        String nama = "naya";
+        String alamat = "kalukku";
+        System.out.println("nama saya "+ nama);
+        System.out.println("saya beralamat di "+ alamat);
+      
+    }
+   
+}
