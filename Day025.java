4e5b7752fad3dcd5fcb6b06ba682e857ea3dffac
Naya <nayarahatmawardani15062006@gmail.com>
2024-09-26 08:45:46 +0800

    Create Day025.java

---
+++ Day025.java
@@ -0,0 +1,10 @@
+package Day25;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      Double tb = 148.5;
+     System.out.println ("tinggi badan : " + tb);        
+    }
+    
+}
