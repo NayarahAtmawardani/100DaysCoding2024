b136b6cec2a8b45a9205b95c556e62baddec58dc
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-03 12:10:25 +0700

    Create Day032.java

---
+++ Day032.java
@@ -0,0 +1,37 @@
+
+public class day032 {
+    public static void main(String[] args) {
+        //bilangan bulat
+        byte a = 18;
+        String naya = Byte.toString(a);
+        System.out.println(naya);
+        short b = 20;
+        String loppy = String.valueOf(b);
+        System.out.println(loppy);
+        int c = 10;
+        String gtw = c + "";
+        System.out.println(gtw);
+        long d = 30;
+        String apaaja = Long.toString(d);
+        System.out.println(apaaja);
+        //bilangan desimal
+        double e = 20.5;
+        String gtw2 = String.valueOf(e);
+        System.out.println(gtw2);
+        float f = 40.1f;
+        String apaya =f + "";
+        System.out.println(f);
+        //character
+        char g = 'A';
+        String terserah = g + "";
+        System.out.println(terserah);
+        //true false
+        boolean h = true;
+        String terserah2 = h + "";
+        System.out.println(terserah2);
+        
+        
+        
+    }
+    
+}
