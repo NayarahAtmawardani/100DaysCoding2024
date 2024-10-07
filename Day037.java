d465c3689561e2a841d958f1bc226af77a5b67ef
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-08 00:21:58 +0700

    Create Day037.java

---
+++ Day037.java
@@ -0,0 +1,15 @@
+
+public class day037 {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 50;
+        
+        x += 20;
+        y -= 30;
+        
+        System.out.println("berapa nilai akhir x stelah penambahan : "+ x);
+        System.out.println("berapa nilai akhir y setelah pengurangan : "+ y);
+                
+    }
+    
+}
