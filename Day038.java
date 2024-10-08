efb32fdf83c711cb48acdb4f73afff4e8d2b834f
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-09 00:01:38 +0700

    Create Day038.java

---
+++ Day038.java
@@ -0,0 +1,11 @@
+
+public class day038 {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 20;
+        
+        System.out.println("hasil perkalian dari x : " + (x *= 2));
+        System.out.println("hasil pembagian dari y : " + (y /= 2));
+    }
+   
+}
