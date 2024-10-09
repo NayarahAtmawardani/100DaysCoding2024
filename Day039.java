70065256dd9ec0f59a9328cdace3f418915b2527
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-10 00:07:50 +0700

    Create Day039.java

---
+++ Day039.java
@@ -0,0 +1,11 @@
+
+public class day39 {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 3;
+        int hasil = x %= y;
+        System.out.println("hasil sisa bagi dari x dan y adalah : " + hasil);
+        
+    }
+    
+}
