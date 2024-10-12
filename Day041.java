89c457287ca75700bcf7b407c28ce764dc185255
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-12 10:36:56 +0700

    Create Day041.java

---
+++ Day041.java
@@ -0,0 +1,17 @@
+
+public class day41 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 15;
+        int c = 20;
+        int d = 10;
+        
+        System.out.println(a == b);
+        System.out.println(a += c);
+        System.out.println(b -= a);
+        System.out.println(b *= c);
+        System.out.println(c /= a);
+        System.out.println(c %= b);
+    }
+    
+}
