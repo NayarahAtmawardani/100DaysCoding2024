12d97193313b498a56cf257e9ab1a8374a7d7d53
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-12 19:39:04 +0700

    Create Day041B.java

---
+++ Day041B.java
@@ -0,0 +1,20 @@
+package EvaluasiMentor2;
+
+import java.util.Scanner;
+
+public class no7 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner (System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      boolean sama = a == b; 
+      boolean tua = a > b;
+      boolean muda = a < b;
+        System.out.println(sama);
+        System.out.println(tua);
+        System.out.println(muda);
+    }
+  
+     
+
+}
