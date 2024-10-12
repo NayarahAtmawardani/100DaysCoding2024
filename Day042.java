edb3a1b17a63b9e4b2f11669ae952d2875635776
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-13 06:09:04 +0700

    Create Day042.java

---
+++ Day042.java
@@ -0,0 +1,13 @@
+
+public class day042 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 6;
+        
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(b != c);
+    }
+    
+}
