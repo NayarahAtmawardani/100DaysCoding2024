a0d442255b7d20bde99adc05ae732903b6f4d4e8
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-15 01:43:36 +0700

    Create Day044.java

---
+++ Day044.java
@@ -0,0 +1,10 @@
+
+public class day044 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 10;
+        System.out.println(a > b && b < c);
+    }
+    
+}
