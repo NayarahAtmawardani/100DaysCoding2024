1c07e2903910366fd56f44ba291cfb84e18c5099
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-16 05:38:09 +0800

    Create Day045.java

---
+++ Day045.java
@@ -0,0 +1,9 @@
+public class day045 {
+    public static void main(String[] args) {
+        int x = 40;
+        int y = 11;
+        int n = 20;
+        System.out.println(x != y || y == n);
+    }
+    
+}
