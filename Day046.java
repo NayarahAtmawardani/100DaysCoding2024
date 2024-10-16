137e1e5d38958bceff30d1f19a3af77adfb91dab
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-17 03:47:38 +0800

    Create Day046.java

---
+++ Day046.java
@@ -0,0 +1,9 @@
+public class day046 {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        System.out.println(!x || y);
+        System.out.println(!x || !y);
+    }
+    
+}
