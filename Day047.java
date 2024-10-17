2884e9339416c434075b75523bda7c7fc61cf47a
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-17 23:53:27 +0700

    Create Day047.java

---
+++ Day047.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class day047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        
+        if(a % 2 == 1){
+            System.out.println("ganjil");
+        }if (a % 2 == 0) {
+        System.out.println("genap");
+    }
+    }
+    
+}
