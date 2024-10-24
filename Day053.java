e18925c97e725cc6eb4ff397867f05cc77ad9ac3
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-24 15:58:44 +0700

    Update Day053.java

--- Day053.java
+++ Day053.java
@@ -1,14 +1,15 @@
+
 import java.util.Scanner;
 
 
 public class day053 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int N = sc.nextInt();
+        int finish = sc.nextInt();
         
-        for(int a = 1 ; a < N ; a++){
-            if(a % 3 != 0 && a % 2 == 0){
-                System.out.println(a);
+        for(int start = 1 ; start < finish ; start++){
+            if(start % 3 != 0 && start % 2 == 0){
+                System.out.print(start + " ");
             }
         }
     }
