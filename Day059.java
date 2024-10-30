296cf1e1b4eebb23e6673431120b6b2d5299ffde
Naya <nayarahatmawardani15062006@gmail.com>
2024-10-30 10:19:00 +0700

    Create Day059.java

---
+++ Day059.java
@@ -0,0 +1,11 @@
+
+public class day059 {
+    public static void main(String[] args) {
+        char [] a = {'N','A','Y','A'};
+        
+        for (char b : a ){
+            System.out.println(b);
+         
+        }
+    }
+}
