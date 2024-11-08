aa35e2eea25e961f6de1e97d6b10166ee4704789
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-08 20:26:46 +0700

    Create Day068.java

---
+++ Day068.java
@@ -0,0 +1,14 @@
+
+public class day068 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+        
+        int hasil = tambah (a,b);
+        System.out.println(a+ "+"+b+"="+hasil);
+    }
+    static int tambah (int x,int y){
+        return x + y;
+    }
+    
+}
