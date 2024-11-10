42df668ae66ab7b6cf5547032d3b2b0cf221bae4
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-10 14:53:22 +0700

    Create Day070.java

---
+++ Day070.java
@@ -0,0 +1,10 @@
+
+public class day070 {
+    public static String loppy (String nama,int umur){
+        return nama + " berumur "+umur +" tahun";
+    }
+    public static void main(String[] args) {
+        System.out.println(loppy("naya",17));
+    }
+    
+}
