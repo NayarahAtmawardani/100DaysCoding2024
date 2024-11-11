967503ad3fe85ca693fae771d04c3a434ddb391c
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-11 21:02:54 +0800

    Create Day071.java

---
+++ Day071.java
@@ -0,0 +1,9 @@
+public class Day071 {
+    public static void main(String[] args) {
+        System.out.println("HEllO WORLD!!");
+        System.out.println(nonMain(5));
+    }
+    public static int nonMain(int a){
+        return a;
+    }
+}
