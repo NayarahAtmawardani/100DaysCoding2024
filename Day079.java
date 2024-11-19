46ce2de1b11584b477c9b30dc97c0310aa93c83e
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-19 19:03:25 +0700

    Create Day079.java

---
+++ Day079.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+
+public class day079 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        String b = Integer.toString(a);
+        System.out.println("hasil : "+ b);
+    }
+    
+}
