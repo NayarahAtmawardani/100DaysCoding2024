1d1db0e876212a9da9841a03e0af53d4b5977ff9
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-20 19:54:17 +0800

    Create Day080.java

---
+++ Day080.java
@@ -0,0 +1,10 @@
+public class DAY80 {
+
+    public static void main(String[] args) {
+        String a = "\t Alloooow guys!!!! \t";
+
+        System.out.println(a); //sebelum outputnya (     Alloooow guys!!!!    )
+        System.out.println(a.trim()); //sesudah  (Alloooow guys!!!!)
+    }
+
+}
