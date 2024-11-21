bf61b406c17b1d999ef3b7cae5dfc4b4a1d83563
Naya <nayarahatmawardani15062006@gmail.com>
2024-11-21 20:40:30 +0800

    Create Day081.java

---
+++ Day081.java
@@ -0,0 +1,11 @@
+public class day081 {
+    public static void main (String[]args){
+        int angka = 20;
+        System.out.println(String.valueOf(angka));
+        
+        
+        
+        
+    }
+    
+}
